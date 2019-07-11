2fe5bc4196adb7319281121e9d92d26ec390d090
Santosh Mahto <mahtos480@gmail.com>
2019-07-11 10:47:59 +0530

    created example

---
+++ examples/template engine/index.js
@@ -0,0 +1,55 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('@koa/cors');
+const static = require('koa-static');
+const render = require('koa-ejs');
+const path = require('path');
+
+const app = new Koa();
+const router = new Router();
+
+const livereload = require('../../lib');
+
+// importing views
+const INDEX = 'index';
+
+// set port
+const PORT = process.env.PORT || 80;
+
+// allow cors request
+app.use(cors());
+
+// serve static file as a public
+app.use(static(path.join(__dirname, '/public')));
+
+// view engine with ejs rendering
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'ejs',
+  delimiter: '%',
+  cache: false,
+  debug: false,
+  async: true,
+});
+
+/**
+ * @description configure koa-liverefresh
+ */
+livereload(router);
+
+// root get request handler
+router.get('/', async (ctx) => {
+  let sse = ``;
+  await ctx.render(INDEX,{
+    sse
+  });
+});
+
+// attached router object with app
+app.use(router.routes()).use(router.allowedMethods());
+
+// listen to the server
+app.listen(PORT, () => {
+  console.log(`server is listening port: ${PORT}`);
+});
